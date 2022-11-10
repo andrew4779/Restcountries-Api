3aafd578649c4477aa1b2984d9f06aaac8312773
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-11-10 09:04:51 +0300

    Style button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
-
 export default function Button(props){
     return (
         <button
